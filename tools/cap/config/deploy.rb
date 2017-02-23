5851562a27418aec88833ad694def7913a066a8d
David Alger <davidmalger@gmail.com>
2017-02-23 12:12:43 -0600

    Updated capistrano configuration

--- tools/cap/config/deploy.rb
+++ tools/cap/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '~> 3.6'
+lock '~> 3.7'
 
 set :application, 'm2cap_demo'
 set :repo_url, 'git@github.com:davidalger/m2-cap.git'
@@ -7,11 +7,8 @@ set :keep_releases, 1
 set :format_options, command_output: :stderr
 
 set :deploy_to, '/home/vagrant/sites/m2cap.demo'
-set :branch, ENV['BRANCH'] || "master"
+set :branch, ENV['BRANCH'] || "2.1"
 
 # set :magento_deploy_production, false
-set :magento_deploy_maintenance, false
+# set :magento_deploy_maintenance, false
 set :magento_deploy_themes, ['Magento/blank', 'Magento/backend']
-
-# Load custom tasks from `lib/capistrano/tasks` if you have any defined
-Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
